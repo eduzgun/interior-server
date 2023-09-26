ce8bef7dff27aed17724f20b9fd539c52f4ec78c
Valentin Abrutin <abrutinvalic@gmail.com>
2023-09-26 16:01:35 +0100

    changed main endpoint to be in app file

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
+from flask import jsonify
 from application import app
 from application.blueprints.users.users import users_bp
 from application.blueprints.rooms.rooms import rooms_bp
 from application.blueprints.likes.likes import likes_bp
 from application.blueprints.auth.auth import auth_bp
 
+@app.route("/")
+def hello_interiordesign():
+    return jsonify({
+        "message": "Welcome to Interior Design API",
+        "description": "Interior Design API",
+        "endpoint": [
+            "GET /"
+        ]
+    }), 200
 
 app.register_blueprint(users_bp)
 app.register_blueprint(rooms_bp)
