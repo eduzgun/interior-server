efa0058df0819c567ccfd2e745e595a4baf6c62b
eduzgun <emreduz00@gmail.com>
2023-09-26 10:20:34 +0100

    test rooms setup

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,27 +1,29 @@
 
-import pytest
-from application import app
-from application.blueprints.users.users import users_bp
 
-@pytest.fixture(scope="module")
-def client():
-    # Create a test client using the Flask application configured for testing
-    app.config['TESTING'] = True
-    #add the user blueprint
-    app.register_blueprint(users_bp)
+def test_home_route(test_client):
+    '''
+    GIVEN a Flask app is defined
+    WHEN the '/' route is requested (GET)
+    THEN check that the response is valid
+    '''
+    response = test_client.get("/")
+    assert response.status_code == 200
 
-    client = app.test_client()
-    # this is where the test runs
-    yield client
-
-def test_get_all_users(client):
+def test_get_all_users(test_client):
     '''
     GIVEN the GET /users route is defined
     WHEN the '/users' page is requested (GET)
     THEN check that the response is valid
     '''
-    response = client.get('/users')
+    response = test_client.get('/users')
     assert response.status_code == 200
     
 
-
+def test_handle_internal_server_error(test_client):
+    '''
+    GIVEN the app is configured with error-handling routes
+    WHEN a route that triggers an error is requestd e.g. /tupperware
+    THEN check the response is an error
+    '''
+    response = test_client.get('/tupperware')
+    assert response.status_code == 404
