5640c0f35a35d512320f5961e7110b678d56960f
eduzgun <emreduz00@gmail.com>
2023-09-28 16:23:49 +0100

    changing backend

--- app.py
+++ app.py
@@ -1,16 +1,5 @@
-from flask import jsonify
 from application import create_app
-
 app = create_app()
-@app.route("/")
-def hello_interiordesign():
-    return jsonify({
-        "message": "Welcome to Interior Design API",
-        "description": "Interior Design API",
-        "endpoint": [
-            "GET /"
-        ]
-    }), 200
 
 
 if __name__ == "__main__":
