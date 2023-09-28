e5d360dd50c2a02358a8be73cea2dda55864a2b3
Valentin Abrutin <abrutinvalic@gmail.com>
2023-09-28 22:00:40 +0100

    minor merging fix

--- application/__init__.py
+++ application/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from flask_session import Session
@@ -20,6 +20,16 @@ def create_app():
     app.json_provider_class.sort_keys = False
     CORS(app)
 
+    @app.route("/")
+    def hello_interiordesign():
+        return jsonify({
+            "message": "Welcome to Interior Design API",
+            "description": "Interior Design API",
+            "endpoint": [
+                "GET /"
+            ]
+        }), 200
+
 
     db.init_app(app)
 
@@ -29,11 +39,13 @@ def create_app():
         from application.blueprints.rooms.rooms import rooms_bp
         from application.blueprints.auth.auth import auth_bp
         from application.blueprints.likes.likes import likes_bp
+        from application.blueprints.filestorage.filestorage import filestorage_bp
 
         app.register_blueprint(users_bp)
         app.register_blueprint(rooms_bp)
         app.register_blueprint(auth_bp)
         app.register_blueprint(likes_bp)
+        app.register_blueprint(filestorage_bp)
     
         
         db.create_all()
